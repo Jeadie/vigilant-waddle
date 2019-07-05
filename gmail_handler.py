409cee1366192aea8ddef69cb6050de793b60d57
Jack Eadie <jack.eadie0@gmail.com>
2019-07-05 20:24:41 +1000

    clean up docs. Finish separating API handler and user controller.

--- gmail_handler.py
+++ gmail_handler.py
@@ -1,109 +1,89 @@
-import json 
-import email
 import base64
+import email
+import logging
 import os
-from typing import List
+from typing import List, Dict
 
 from oauth2client.file import Storage
 from apiclient.discovery import build
 import html2text
-import time
 
-DEFAULT_CREDENTIAL_PATH="credentials-je.dat"
-class CommandHandler(object):
-
-    def __init__(self, gmail):
-        self.gmail = gmail
-
-    def recent(self, args):
-        number = 10 if len(args) < 2 else args[1]
-        number = int(number)
-        self.gmail.process_email_list("category:primary", max_messages=number)
-
-    def list(self, args):
-        if len(args) < 2:
-            print("Please provide query with this command. I.e. `list 'category:primary'`")
-        else:
-            query = args[1]
-            self.gmail.process_email_list(query, max_messages=None if len(args)< 3 else args[2])
-
-    def read(self, args):
-        if len(args) < 2:
-            print("Please provide the index of email to read.")
-        else:
-            try:
-                number = int(args[1])
-                self.gmail.read_message(number)
-
-            except ValueError as e:
-                print(f"The value {args[1]} is not an integer.")
-
-            except IndexError as e:
-                print(f"Index {number} is out of range.")
-
-    def back(self, args):
-        self.gmail.print_previous_list()
+DEFAULT_CREDENTIAL_PATH = "credentials-je.dat"
+_logger = logging.getLogger(__name__)
 
 
-    def help(self, gmail):
-        print(
-                        """
-    Not a valid command. Commands: 
-    `recent [int]`: Lists last [int] emails from main inbox. Default 10.
-    `list [query] [int]`: Lists the first [int] emails that match the query [query].
-                            If no int is provided, all emails matching the query are returned.
-    `read [int]`: Reads the indexed [int] from the previous list. [int] must be
-                    less than the number of emails in list. 
-    `back`: Prints the previous email list.
-                        """
-                    )
+class GmailHandler(object):
+    """
+        A custom client to handle authentication and common requests of the
+        gmail API.
+    """
 
-
-
-class GmailHandler(object): 
-    
     def __init__(self, cred_file):
+        """
+
+        Args:
+            cred_file: An credential file for the oauth2 client.
+
+        Returns:
+             Constructor.
+        """
         self.cred = Storage(cred_file).get()
         self.service = build('gmail', 'v1', credentials=self.cred)
         self.message_list = []
 
-    def get_message_from_id(self, id:str, form:str="raw", metadata:List[str]=None):
+    def get_message_from_id(self, id: str, form: str = "raw",
+                            metadata: List[str] = None) -> Dict[str, str]:
         """ Gets a Message object, given its ID.
 
-        :param id: Id of a email.
-        :param form: The format of email to return, options are: 'full', 'metadata', 'minimal', 'raw'.
-                        See https://developers.google.com/gmail/api/v1/reference/users/messages/get
-        :return: A Message Object.
+        Args:
+            id: Id of a email.
+            form: The format of email to return, options are: 'full', 'metadata', 'minimal', 'raw'.
+                            See https://developers.google.com/gmail/api/v1/reference/users/messages/get
+            metadata: metadata headers to include when receiving messages.
+
+        Returns:
+            A message Object.
+
+        Raises:
+            TBD
         """
         if metadata:
-            return self.service.users().messages().get(userId="me", id=id, format=form, metadataHeaders=metadata).execute()
+            return self.service.users().messages().get(userId="me", id=id,
+                                                       format=form,
+                                                       metadataHeaders=metadata).execute()
         else:
-            return self.service.users().messages().get(userId="me", id=id, format=form).execute()
+            return self.service.users().messages().get(userId="me", id=id,
+                                                       format=form).execute()
 
-    def process_email_list(self, query:str, max_messages:int=None):
-        """ Prints the snippets for a set of emails, following a given query.
+    def print_email_list(self, emails):
+        """ Prints a list of email previews, including the name of the sender
+            and a snippet of the message.
 
-        :param query: A query string to filter emails with. Same format as string
-            filtering in the gmail GUI.
-        :param max_messages: The maximum amount of messages to retrieve and print.
+        Args:
+            emails: A list of message objects from the Gmail API.
+
+        Return:
+            True if the list of emails was successfully sent to stdout, False otherwise.
         """
-        messages = self.get_messages_from_query(query, form="metadata", metadata=["From", "Message-ID"], max_messages=max_messages)
+        try:
+            for m, i in zip(emails, range(len(emails))):
+                From = m["payload"]["headers"]
+                From = next(x["value"] for x in From if x["name"] == "From")
+                From = f"{(45 - len(From)) * ' '}{From}"
+                index = f"{((len(self.message_list) // 10) - (i // 10)) * ' '}{i}"
+                print(f"|{index}|{From} | {m['snippet'][:140]} ")
+        except KeyError as e:
+            _logger.error(
+                f"An Gmail message object did not have expected keys. Error: {e}."
+            )
+            return False
+        else:
+            return True
 
-        self.message_list = messages
-        print(f"## Emails from Query: `{query}`")
-        self.print_previous_list()
-        if max_messages:
-            print(f"NOTE: Only the top {max_messages} messages were retrieved. There may be more.")
-
-    def print_previous_list(self):
-        for m, i in zip(self.message_list, range(len(self.message_list))):
-            From = m["payload"]["headers"]
-            From = next(x["value"] for x in From if x["name"] == "From")
-            From = f"{(45-len(From))*' '}{From}"
-            index = f"{((len(self.message_list)//10)-(i//10))*' '}{i}"
-            print(f"|{index}|{From} | {m['snippet'][:140]} ")
-
-    def get_messages_from_query(self, query, form:str="raw", metadata:List[str]=None, max_messages:int=None)->None:
+    def get_messages_from_query(self, query, form: str = "raw",
+                                metadata: List[str] = None,
+                                max_messages: int = None) -> List[
+        Dict[str, str]]:
         """ Returns complete message objects for a single query.
 
         :param query: A query string to filter emails with. Same format as string
@@ -115,29 +95,32 @@ class GmailHandler(object):
                              the filter will be returned.
         :return: A list of Message objects filtered by the given query.
         """
-        messages = self.service.users().messages().list(userId="me", q=query).execute()
+        messages = self.service.users().messages().list(userId="me",
+                                                        q=query).execute()
         if max_messages:
-            return [self.get_message_from_id(m["id"], form=form, metadata=metadata) for m in messages["messages"][0:max_messages]]
+            return [
+                self.get_message_from_id(m["id"], form=form, metadata=metadata)
+                for m in messages["messages"][0:max_messages]]
         else:
-            return [self.get_message_from_id(m["id"], form=form, metadata=metadata) for m in messages["messages"]]
-        
-    def read_message(self, index:int)->None:
+            return [
+                self.get_message_from_id(m["id"], form=form, metadata=metadata)
+                for m in messages["messages"]]
+
+    def read_message(self, message: Dict[str, str]) -> bool:
         """ Given an index, prints the corresponding message from the previous list.
 
-        :param index: The index of email from the previously queried list.
+        Args:
+            message: the message object to be printed, in detail with
+
+        Returns:
+            True, if it was successful in displaying the email. False otherwise.
+
         """
-        if index >= len(self.message_list):
-            raise IndexError()
-
-        else:
-            message = self.get_message_from_id(self.message_list[index]["id"])
-            self.print_message(message)
-
-    def print_message(self, message)->None:
-        """ Prints a single email.
-
-        :param message: A gmail Message object.
-        """
+        # TODO: Think of a better way to represent individual emails.
+        #  The current, dumps the html out to stdout.
+        # Ideas:
+        #   Render in browser, pop-up.
+        #   Filter html tags.
         msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
 
         mime_msg = email.message_from_string(msg_str.decode())
@@ -145,12 +128,148 @@ class GmailHandler(object):
         print(f"Date: {mime_msg['Date']}")
         h = html2text.HTML2Text()
         h.ignore_links = True
-        
+
         if mime_msg.is_multipart():
             for payload in mime_msg.get_payload():
                 print(h.handle(payload.get_payload(decode=False)))
         else:
             print(h.handle(mime_msg.get_payload(decode=False)))
+
+
+class GmailController(object):
+    """ A controller for a user interacting with Gmail. Current commands include:
+            - Get a list of emails, given a query.
+            - Read an email's content.
+            - Basic navigation between viewing an email and backtracking to
+                the previous query.
+
+    """
+
+    def __init__(self, gmail: GmailHandler):
+        """
+
+        Args:
+            gmail: A gmail handler to directly interact with Gmail.
+        """
+        self.gmail = gmail
+        self.messages = []
+
+    def _command_pattern(self, args: List[str]) -> bool:
+        """ The function pattern for user commands to follow.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        _logger.warning(
+            f"_command_pattern is the design pattern. It is an interface that"
+            f"should not be called. Args: {args}"
+        )
+        return False
+
+    def recent(self, args: List[str]) -> bool:
+        """ The function pattern for user commands to follow.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        number = 10 if len(args) < 2 else args[1]
+        number = int(number)
+        self.gmail.process_email_list("category:primary",
+                                      max_messages=number)
+
+    def list(self, args: List[str]) -> bool:
+        """ The function pattern for user commands to follow.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        if len(args) < 2:
+            print(
+                "Please provide a query with this command. I.e. `list 'category:primary'`")
+        else:
+            query = args[1]
+            messages = self.gmail.get_messages_from_query(query, max_messages=None if len(
+                args) < 3 else args[2])
+            return self.gmail.print_email_list(messages)
+
+    def read(self, args: List[str]) -> bool:
+        """ Displays a single email to the user.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        if len(args) < 2:
+            print("Please provide the index of email to read.")
+            return False
+        else:
+            try:
+                index = int(args[1])
+                self.gmail.read_message(self.messages[index])
+
+            except ValueError as e:
+                print(f"The value {args[1]} is not an integer.")
+                return False
+
+            except IndexError as e:
+                print(f"Index greater than number of messages. {index}>={len(self.messages)}")
+                return False
+            else:
+                return True
+
+    def back(self, args: List[str]) -> bool:
+        """ Prints the previous message list requested by the used.
+
+        Default use case is after a user has read a specific email.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        if len(args) > 1:
+            return False
+        else:
+            return self.gmail.print_email_list(self.messages)
+
+    def help(self, args: List[str]) -> bool:
+        """ Prints a help message outlining the capaiblities of the tool.
+
+        Args:
+            args: User specified inputs such that args[0] is the command
+                name itself.
+
+        Return:
+            True, if the use input was able to be processed, False otherwise.
+        """
+        print(
+            """
+Not a valid command. Commands: 
+`recent [int]`: Lists last [int] emails from main inbox. Default 10.
+`list [query] [int]`: Lists the first [int] emails that match the query [query].
+                If no int is provided, all emails matching the query are returned.
+`read [int]`: Reads the indexed [int] from the previous list. [int] must be
+        less than the number of emails in list. 
+`back`: Prints the previous email list.
+            """
+        )
 
     @staticmethod
     def run():
@@ -160,25 +279,26 @@ class GmailHandler(object):
         """
         if os.path.exists(DEFAULT_CREDENTIAL_PATH):
             gmail = GmailHandler(DEFAULT_CREDENTIAL_PATH)
-            commands = CommandHandler(gmail)
+            commands = GmailController(gmail)
 
-            should_continue = True
-            while(should_continue):
+            i_should_continue = True
+            while (i_should_continue):
                 i = input("Gmail: ")
                 args = i.split()
                 if len(args) == 0:
                     commands.help(gmail)
                 else:
-                    {"recent": commands.recent, 
-                     "list": commands.list, 
-                     "read": commands.read,
-                     "back": commands.back}.get(args[0], commands.help)(args)
+                    {"recent": commands.recent,
+                       "list": commands.list,
+                       "read": commands.read,
+                       "back": commands.back}.get(args[0], commands.help)(args)
         else:
-            print(f"Credentials not at default path: {DEFAULT_CREDENTIAL_PATH}.")
+            print(
+                f"Credentials not at default path: {DEFAULT_CREDENTIAL_PATH}.")
             print("Will require web login.")
-            raise NotImplementedError("Need to integrate web auth script into CLI")
-            
+            raise NotImplementedError(
+                "Need to integrate web auth script into CLI")
+
 
 if __name__ == '__main__':
     GmailHandler.run()
-    
